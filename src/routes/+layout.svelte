00d3fc20df51ace6ca0451798eb4b0ce91959bfb
Oliver Abreu <oliverabreu@icloud.com>
2023-06-23 22:52:20 -0400

    got tailwindcss working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<div class="flex items-center w-full h-50 bg-blue-600" >
+<h5 class="text-[4rem] tracking-wide" style="line-height: 96%;">Tow<br/><span class="font-bold">Rescue</span></h5>
+</div>
+<slot />
+<h6>Footer</h6>
